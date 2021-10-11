ea6630760d9e3493b8978c9800d99e7528f074b9
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2021-10-11 19:45:42 -0300

    dev and production settings

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("my_app")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => { //reques y response, esto es una ruta inicial
+  res.send('Hello World! mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) =>{
+  res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products', (req, res) =>{
+  res.json({
+    name: 'Product',
+    price: 1000
+  });
+});
+
+app.listen(port,()=>{
+  console.log(`Mi port es: ' + ${port}`)
+});
