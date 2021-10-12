d2e50c19427236eadb5c9f34e6575ebb5514ba23
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2021-10-11 22:14:05 -0300

    faker products

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -10,13 +11,57 @@ app.get('/nueva-ruta', (req, res) =>{
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res) =>{
-  res.json({
-    name: 'Product',
-    price: 1000
+app.get('/products', (req, res) =>{ //retorna un array de productos
+  //despues instalamos faker y creamos nuevos productos.
+  const products =[];
+  for (let index = 0; index < 100; index++) {
+    products.push({
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(),10), // viene como string y lo quiero como número.
+        image: faker.image.imageUrl(),
+      })
+    }
+    res.json(products);
   });
+
+
+
+
+
+app.get('/products/:id', (req, res) =>{
+  // const id = req.params.id;
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'Product2',
+    price: 2000
+
+})
 });
 
+app.get('/categories/:categoryId/products/:productId', (req, res)=>{
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId,
+  })
+});
+
+/* Query params */
+
+app.get('/users', (req, res)=>{
+  const {limit, offset} = req.query;
+  if (limit&&offset){
+    res.json({
+      limit: limit,
+      offset: offset
+    });
+  } else {
+    res.send('No hay parametros')
+  }
+});
+/* Instalamos faker en -D  nos permite generar data fake*/
+/* Escucha en el puerto */
 app.listen(port,()=>{
   console.log(`Mi port es: ' + ${port}`)
 });
